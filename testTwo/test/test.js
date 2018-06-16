30142eff0d8e597fe136031c9014275ed2b3c4a2
Andreas Karpasitis <karpasitis@outlook.com>
2018-06-16 20:11:46 +0200

    implement basic mocha tests

---
+++ testTwo/test/test.js
@@ -0,0 +1,17 @@
+const chai = require("chai")
+const should = chai.should()
+const getIndex = require("../utils/indexOf.js")
+
+it('should return correct index (1) for b in string "abcdef"', function(){
+  const testString = 'abcdef'
+  const index = testString.getIndex('b', 0).index
+  index.should.equal(1)
+})
+
+it('should start from correct index (3), ignore previous positions for b in string "bbbbbb" and return 3', function(){
+  const testString = 'bbbbbb'
+  const index = testString.getIndex('b', 3).index
+  const indexZero = testString.getIndex('b', 0).index
+  index.should.equal(3)
+  indexZero.should.equal(0)  
+})
